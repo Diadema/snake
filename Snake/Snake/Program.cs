8fb5f21b4011ac0bbaf7d15b047f473f0c80d70d
Diadema <Life93105@gmail.com>
2016-11-20 16:56:56 +0300

    Класс список

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,9 +13,28 @@ namespace Snake
             Point p1 = new Point(1, 3, '*');
             p1.Drow();
 
-
             Point p2 = new Point(4, 5, '#');
               p2.Drow();
+
+
+              List<int> numList = new List<int>();
+              numList.Add(0);
+              numList.Add(1);
+              numList.Add(2);
+
+              int x = numList[0];
+              int y = numList[1];
+              int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.Remove(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
 
